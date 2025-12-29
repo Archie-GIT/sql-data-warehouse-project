374c90a1b525174aa393b255494743a757cf89d2
Archie-GIT <jainarchi37@gmail.com>
2025-12-29 10:38:09 +0530

    Create int_database.sql

---
+++ scripts/int_database.sql
@@ -0,0 +1,25 @@
+USE master;
+GO
+
+  --Drop & recreate the'Datawarehouse' database
+  IF EXIST (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
+  BEGIN
+    ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Datawarehouse;
+END;
+GO
+
+  --Create the'Datawarehouse' database
+
+CREATE DATABASE  Datawarehouse
+GO
+USE DataWarehouse;
+GO
+
+  -- Create schemas
+  
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO 
+CREATE SCHEMA gold;
